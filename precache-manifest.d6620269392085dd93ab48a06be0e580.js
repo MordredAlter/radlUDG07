79d7662b174027b5881b6e3525afb6fcbb628a37
Ricardo Diaz <RicardoDHylian@hotmail.com>
2019-03-22 20:30:36 -0600

    Updates

---
+++ precache-manifest.d6620269392085dd93ab48a06be0e580.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42724d510a9b1536e3ce",
+    "url": "/radlUDG07/static/js/runtime~main.5e8fc845.js"
+  },
+  {
+    "revision": "a83614386f65d7c4650c",
+    "url": "/radlUDG07/static/js/main.7015cb2a.chunk.js"
+  },
+  {
+    "revision": "37235479df2df431c194",
+    "url": "/radlUDG07/static/js/2.e7aabdc3.chunk.js"
+  },
+  {
+    "revision": "a83614386f65d7c4650c",
+    "url": "/radlUDG07/static/css/main.0d4c414c.chunk.css"
+  },
+  {
+    "revision": "07da8db67442fcf95ccf21592b1d133d",
+    "url": "/radlUDG07/index.html"
+  }
+];
